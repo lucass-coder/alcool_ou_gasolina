e1f82822d67a8d1f498df1ee1e847833e4b6b28e
Lucas Santana <lsantana.2015@alunos.utfpr.edu.br>
2021-08-08 21:02:36 -0300

    Criados os métodos para calculo dos combustíveis

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,17 +1,43 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
-
-
-
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-
   TextEditingController _alcoolController = TextEditingController();
   TextEditingController _gasolinaController = TextEditingController();
+  String _textoResultado = "";
+
+  void _calcular() {
+    double precoAlcool = double.tryParse(_alcoolController.text);
+    double precoGasolina = double.tryParse(_gasolinaController.text);
+
+    if (precoAlcool == null || precoGasolina == null) {
+      setState(() {
+        _textoResultado = "Número inválido, digite números maiores que 0";
+      });
+    } else {
+      if ((precoAlcool / precoGasolina) >= 0.7) {
+        setState(() {
+          _textoResultado = "Melhor abastecer com Gasolina";
+        });
+      } else {
+        setState(() {
+          _textoResultado = "Melhor abastecer com Alcool";
+        });
+      }
+
+      //MÉTODO QUE LIMPARIA OS CAMPOS AO APERTAR O BOTÃO
+      //_limparCampos();
+    }
+  }
+  
+  void _limparCampos() {
+    _alcoolController.text = "";
+    _gasolinaController.text = "";
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,13 +52,14 @@ class _HomeState extends State<Home> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Padding(padding: EdgeInsets.only(bottom: 32),
-                child: Image.asset("images/logo.png"),
-              ),
-
               Padding(
                 padding: EdgeInsets.only(bottom: 32),
-                child: Text("Saiba qual a melhor opção para abastecimento do seu carro",
+                child: Image.asset("images/logo.png"),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 32),
+                child: Text(
+                  "Saiba qual a melhor opção para abastecimento do seu carro",
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 24,
@@ -64,24 +91,25 @@ class _HomeState extends State<Home> {
               Padding(
                 padding: EdgeInsets.only(top: 32),
                 child: RaisedButton(
-                  child: Text("Calcular",
-                  style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.white,
+                  child: Text(
+                    "Calcular",
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.white,
                     ),
                   ),
                   color: Colors.blueAccent,
-                  onPressed: () {},
+                  onPressed: _calcular,
                 ),
               ),
               Padding(
                 padding: EdgeInsets.only(top: 20),
                 child: Text(
-                  "Resultado",
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
-                      ),
+                  _textoResultado,
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
             ],
